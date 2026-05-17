6890d3aa64811b33e822b6a5fe3b023fbcc88a53
JP Van Sloun <jpvansloun@gmail.com>
2026-05-16 21:07:13 -0500

    Added board initialization logic.

--- ChessApp/Models/Board.cs
+++ ChessApp/Models/Board.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using ChessApp.Models.Pieces;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Data.Common;
+using Tmds.DBus.Protocol;
 
 namespace ChessApp.Models
 {
@@ -8,11 +10,51 @@ namespace ChessApp.Models
     {
         public Board()
         {
+            BoardState = new Dictionary<string, Square>();
+            initBoard();
+        }
+
+        private void initBoard()
+        {
+            string[] columns = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+            foreach (string column in columns)
+            {
+                for (int i = 1; i <= 8; i++)
+                {
+                    string notation = $"{column}{i}";
+                    BoardState.Add(notation, new Square(notation, null));
+                }
+            }
+
+            Piece[] whiteBackLine = createBackLine(PlayerColor.White);
+            Piece[] blackBackLine = createBackLine(PlayerColor.Black);
+
+            for (int i = 0; i < columns.Length; i++)
+            {
+                BoardState[$"{columns[i]}2"].Piece = new Pawn(PlayerColor.White);
+                BoardState[$"{columns[i]}7"].Piece = new Pawn(PlayerColor.Black);
+
+                BoardState[$"{columns[i]}1"].Piece = whiteBackLine[i];
+                BoardState[$"{columns[i]}8"].Piece = blackBackLine[i];
+            }
+        }
+
+        private Piece[] createBackLine(PlayerColor playerColor)
+        {
+            return [new Rook(playerColor),
+                    new Knight(playerColor),
+                    new Bishop(playerColor),
+                    new Queen(playerColor),
+                    new King(playerColor),
+                    new Bishop(playerColor),
+                    new Knight(playerColor),
+                    new Rook(playerColor)];
         }
 
         public Dictionary<string, Square> BoardState { get; }
 
-        public bool movePiece(string square, PlayerColor color)
+        public bool movePiece(string srcSquare, string dstSquare, PlayerColor color)
         {
             return false;
         }
