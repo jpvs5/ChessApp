a42137489a782252a9f011a1bb45d8eb65f088c8
JP Van Sloun <jpvansloun@gmail.com>
2026-05-18 17:38:55 -0500

    Updated Pawn validMove to override new Piece signature and implement base test for 1 square movement

---
+++ ChessApp.Tests/PawnTests.cs
@@ -0,0 +1,19 @@
+﻿using ChessApp.Models;
+using ChessApp.Models.Pieces;
+using SkiaSharp;
+
+namespace ChessApp.Tests
+{
+    public class PawnTests
+    {
+        [Fact]
+        public void MovePawn_ForwardOne_ReturnTrue()
+        {
+            Pawn pawn = new Pawn(PlayerColor.White);
+
+            bool result = pawn.validMove("a2", "a3");
+
+            Assert.True(result);
+        }
+    }
+}
